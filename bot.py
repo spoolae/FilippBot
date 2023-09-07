27e0c355b0f21ba5930694be1cee068a1157db34
Spoolae <ohotnic147feed@gmail.com>
2023-09-07 18:03:54 +0200

    Separate token

--- bot.py
+++ bot.py
@@ -1,16 +1,15 @@
 import logging
-
 from aiogram import Bot, Dispatcher, executor, types
 
-API_TOKEN = '6466586665:AAEp-urViYXkfeXj_SK17ISVM5RQbIr7Bho'
+# Импортируем токен из файла config.py
+from config import TELEGRAM_TOKEN
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
 
 # Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
-
 
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
@@ -19,15 +18,9 @@ async def send_welcome(message: types.Message):
     """
     await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
-
-
 @dp.message_handler()
 async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
-
     await message.answer(message.text)
-
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
